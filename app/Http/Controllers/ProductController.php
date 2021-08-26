04fcbed697c1bca0cf4570ac6116e488388aab8b
wagnerbagonza <wagnerferreira.bgz@gmail.com>
2021-08-26 11:14:30 -0300

    Sobe poha

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return Product::all();
+    }
+
+    public function store(Request $resquest)
+    {
+        $resquest->validate([
+            'name' => 'required',
+            'slug' => 'required',
+            'price' => 'required'
+        ]);
+        return Product::create($resquest->all());
+    }
+
+    public function show($id)
+    {
+        return Product::find($id);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $product = Product::find($id);
+        $product->update($request->all());
+    }
+
+    public function destroy($id)
+    {
+        return Product::destroy($id);
+    }
+
+    public function search($name)
+    {
+        return Product::where('name','like','%'.$name.'%')->get();
+    }
+}
